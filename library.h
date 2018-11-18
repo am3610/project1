a60b8eec155909d6b344a2a28bbaed708e8de0df
Hyeonho Kim <am3610@naver.com>
2018-11-18 20:13:59 +0900

    Skeleton code for space input in library.cpp. Add format of check_8 ~ check_14.

--- library.h
+++ library.h
@@ -4,8 +4,10 @@
 #include <set>
 #include <map>
 #include <fstream>
+#include <vector>
 #include "member.h"
 #include "resource.h"
+#include "space.h"
 #include "date.h"
 
 struct opset{
@@ -15,6 +17,17 @@ struct opset{
 	std::string o;
 	std::string m_t;
 	std::string m_n;
+};
+
+struct spset{
+	std::string d;
+	std::string s_t;
+	std::string s_n;
+	std::string o;
+	std::string m_t;
+	std::string m_n;
+	std::string n_m;
+	std::string t;
 };
 
 class library{
@@ -27,11 +40,17 @@ class library{
 		std::map<std::string, book*> books;
 		std::map<std::string, ebook*> ebooks;
 		std::map<std::string, std::map<std::string, magazine*> > magazines;
+
+		std::map<std::string, seat*> f1; 
+		std::map<std::string, seat*> f2; 
+		std::map<std::string, seat*> f3;
+		std::vector<studyRoom*> rooms;
 		
 		void resManager(std::string, std::ofstream&);
 		void spcManager(std::string, std::ofstream&);
 
 		void mem_add(std::string, std::string);
+
 		bool check_1(struct opset, const int, std::ofstream&);
 		bool check_2(struct opset, const int, std::ofstream&);
 		bool check_3(struct opset, const int, std::ofstream&);
@@ -44,6 +63,16 @@ class library{
 
 		void borrowRes(struct opset);
 		void returnRes(struct opset);
+
+		bool check_8(struct spset, const int, std::ofstream&);
+		bool check_9(struct spset, const int, std::ofstream&);
+		bool check_10(struct spset, const int, std::ofstream&);
+		bool check_11(struct spset, const int, std::ofstream&);
+		bool check_12(struct spset, const int, std::ofstream&);
+		bool check_13(struct spset, const int, std::ofstream&);
+		bool check_14(struct spset, const int, std::ofstream&);
+
+		void spcReset(); 
 
 	public:
 		library(char**);
